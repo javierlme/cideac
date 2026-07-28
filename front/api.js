1002ce6c12cc8140a2e565a76ce0d79161c9d5b2
JAVIER SANCHEZ JIMENEZ <javier.sanchez2@leroymerlin.es>
2026-07-28 09:14:39 +0200

    fix: correct baseURL condition in API configuration

--- front/api.js
+++ front/api.js
@@ -1,8 +1,6 @@
 import axios from 'redaxios';
 
-const isDev = process.env.NODE_ENV === 'development';
-
 export const API = axios.create({
-  baseURL: isDev ? 'http://localhost:8081' : 'https://cidead.is-a-teacher.com:8443',
+  baseURL: false?'http://localhost:8081':'https://cidead.is-a-teacher.com:8443', 
   headers: {}
 });
